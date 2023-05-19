f35618c764860de21b40bce3f7f870b004b921b3
Muaz Wazir <muazwazir@gmail.com>
2023-05-20 03:56:17 +0800

    waiter context handler

--- chapter-03/internal/waiter/option.go
+++ chapter-03/internal/waiter/option.go
@@ -1,3 +1,17 @@
 package waiter
 
+import "context"
+
 type WaiterOption func(c *waiterConfig)
+
+func ParentContext(ctx context.Context) WaiterOption {
+  return func(c *waiterConfig) {
+    c.parentCtx = ctx
+  }
+}
+
+func CatchSignals() WaiterOption {
+  return func(c *waiterConfig) {
+    c.catchSignals = true
+  }
+}
